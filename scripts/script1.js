76fabc96e5d4c433a7100dce6a4f182236d93d13
osamaGamalEG <135203999+osamaGamalEG@users.noreply.github.com>
2023-06-11 01:58:31 +0300

    Add files via upload

---
+++ scripts/script1.js
@@ -0,0 +1,15 @@
+function seeMore() {
+  var dots = document.getElementById("basic");
+  var moreText = document.getElementById("plusSection");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
