79d3c61a420b1f34295ccac94b0e6a542baa95a1
osamaGamalEG <135203999+osamaGamalEG@users.noreply.github.com>
2023-06-11 03:46:32 +0300

    script1.js

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,15 +1,18 @@
 function seeMore() {
-  var dots = document.getElementById("basic");
-  var moreText = document.getElementById("plusSection");
+  var basic = document.getElementById("basic");
+  var more = document.getElementById("plusSection");
   var btnText = document.getElementById("myBtn");
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
+  if (more.style.display === "inline") {
+    more.style.display = "none";
+    btnText.innerHTML = "Read More";
+    document.getElementById("basic").scrollIntoView({behavior: 'smooth'});
+
+  } 
+  else {
+   
     btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
+    more.style.display = "inline";
+    document.getElementById("plusSection").scrollIntoView({behavior: 'smooth'});
   }
 }
